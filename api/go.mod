30e5a083746254e8d7c86236382de8bb4dc2fb18
myrovh <admin@myrovh.dev>
2021-06-15 23:47:12 +1000

    api: auth header generation

--- api/go.mod
+++ api/go.mod
@@ -3,6 +3,7 @@ module github.com/myrovh/sms-sender/api
 go 1.16
 
 require (
+	github.com/caarlos0/env/v6 v6.6.2
 	github.com/justinas/alice v1.2.0
 	github.com/rs/zerolog v1.23.0
 )
--- api/main.go
+++ api/main.go
@@ -1,16 +1,30 @@
 package main
 
 import (
+  "bytes"
+  "crypto/hmac"
+  "crypto/sha256"
+  "encoding/base64"
   "flag"
   "fmt"
+  "github.com/caarlos0/env/v6"
   "github.com/justinas/alice"
   "github.com/rs/zerolog"
   "github.com/rs/zerolog/hlog"
   "github.com/rs/zerolog/log"
+  "math/rand"
   "net/http"
+  "net/http/httputil"
   "os"
   "time"
 )
+
+type config struct {
+  ApiKey    string `env:"API_KEY,notEmpty"`
+  ApiSecret string `env:"API_SECRET,notEmpty"`
+}
+
+var cfg config
 
 func main() {
   // parse flags
@@ -18,6 +32,10 @@ func main() {
   port := flag.Int("port", 8080, "sets port to listen on")
   human := flag.Bool("human", false, "if true logger outputs in human readable format")
   flag.Parse()
+
+  if err := env.Parse(&cfg); err != nil {
+    log.Fatal().Err(err).Msg("couldn't parse envs")
+  }
 
   // configure zerolog
   zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
@@ -50,10 +68,70 @@ func main() {
   log.Info().Int("port", *port).Msg("listening")
 
   http.Handle("/check", chain.Then(http.HandlerFunc(healthCheck)))
+  http.Handle("/api/message", chain.Then(http.HandlerFunc(doSms)))
 
   if err := http.ListenAndServe(fmt.Sprintf(":%d", *port), nil); err != nil {
     log.Fatal().Err(err).Msg("error on http listener")
   }
+}
+
+// httpAuth adds a Authorization header to the provided http request using the oauth http mac specification
+// this should be working but get failed to auth responses from sms global api
+// possibly:
+// - hmac hash + conversion to base64 is wrong
+func httpAuth(r *http.Request) (err error) {
+  timestamp := time.Now().Unix()
+  nonce := rand.Int31()
+  macString := fmt.Sprintf("%d\n%d\n%s\n%s\n%s\n433\n\n", timestamp, nonce, r.Method, r.URL.Path, r.URL.Host)
+  fmt.Printf("%s", macString)
+
+  h := hmac.New(sha256.New, []byte(cfg.ApiSecret))
+  h.Write([]byte(macString))
+  mac := base64.StdEncoding.EncodeToString(h.Sum(nil))
+
+  r.Header.Add("Authorization", fmt.Sprintf("MAC id=\"%s\", ts=\"%d\", nonce=\"%d\", mac=\"%s\"", cfg.ApiKey, timestamp, nonce, mac))
+  return nil
+}
+
+// doSms TODO add helper util to close out http response with error message if function fails before expected return
+// test function to try and get communication between program and sms global
+func doSms(w http.ResponseWriter, r *http.Request) {
+  client := &http.Client{}
+
+  req, err := http.NewRequest("GET", "https://api.smsglobal.com/v2/sms/", bytes.NewReader(make([]byte, 100)))
+  if err != nil {
+    log.Error().Err(err).Msg("failure to generate request")
+    return
+  }
+
+  if err := httpAuth(req); err != nil {
+    log.Error().Err(err).Msg("failure to generate oauth header")
+    return
+  }
+
+  req.Header.Add("Content-Type", "application/json")
+  req.Header.Add("Accept", "application/json")
+
+  body, err := httputil.DumpRequest(req, true)
+  if err != nil {
+    log.Error().Err(err).Msg("http request dump")
+  }
+
+  fmt.Printf("%s", body)
+
+  resp, err := client.Do(req)
+  if err != nil {
+    log.Error().Err(err).Msg("http get")
+  }
+  defer resp.Body.Close()
+
+  body, err = httputil.DumpResponse(resp, true)
+  if err != nil {
+    log.Error().Err(err).Msg("http response dump")
+  }
+
+  fmt.Printf("%s", body)
+
 }
 
 // healthCheck writes a plain text response. Simply used to determine if the
